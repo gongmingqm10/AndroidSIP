8f7c6df6d63cc034f8b8b470bc2a92e0914abdf5
gongming <gongmingqm10@foxmail.com>
2014-10-16 14:31:43 +0800

    add register entry in the LoginActivity

--- app/src/main/java/net/gongmingqm10/sipsample/activity/LoginActivity.java
+++ app/src/main/java/net/gongmingqm10/sipsample/activity/LoginActivity.java
@@ -2,10 +2,14 @@ package net.gongmingqm10.sipsample.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import net.gongmingqm10.sipsample.R;
